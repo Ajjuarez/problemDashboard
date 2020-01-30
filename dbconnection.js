6aa9c75e2bf7a92f518970a471568c611501a452
Ajjuarez <60440824+Ajjuarez@users.noreply.github.com>
2020-01-29 19:32:10 -0500

    Update dbconnection.js

--- dbconnection.js
+++ dbconnection.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
  
 // open the database
-let db = new sqlite3.Database('C:/Users/ajjuarez/problemDashboard/sqlite/db/probDashDB.db', sqlite3.OPEN_READWRITE, (err) => {
+let db = new sqlite3.Database('C:/Users/ajjuarez/problemDashboard/problemDashboard/sqlite/db/probDashDB.db', sqlite3.OPEN_READWRITE, (err) => {
   if (err) {
     console.error(err.message);
   }
