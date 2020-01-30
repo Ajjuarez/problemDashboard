5ac6f6c82a6cde86b752ac1251027cc7ecbe519c
Lex Juarez <ajjuarez@iu.edu>
2020-01-30 10:40:02 -0500

    Change log #2, added more features from the prototype, javascript functions in internal pug file not working

--- dbconnection.js
+++ dbconnection.js
@@ -1,13 +1,14 @@
 const sqlite3 = require('sqlite3').verbose();
  
 // open the database
-let db = new sqlite3.Database('C:/Users/ajjuarez/problemDashboard/sqlite/db/probDashDB.db', sqlite3.OPEN_READWRITE, (err) => {
+let db = new sqlite3.Database('C:/Users/ajjuarez/problemDashboard/problemDashboard/sqlite/db/probDashDB.db', sqlite3.OPEN_READWRITE, (err) => {
   if (err) {
     console.error(err.message);
   }
   console.log('Connected to the Problem Dashboard Database.');
 });
  
+//query the database
 db.parallelize(() => {
   db.each(`SELECT contact_id as id,
                 first_name as name,
@@ -27,3 +28,4 @@ db.close((err) => {
   }
   console.log('Close the database connection.');
 });
+
