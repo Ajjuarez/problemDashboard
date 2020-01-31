ac75ac2a1ecb6545dfed950bd506049c051fc753
Lex Juarez <ajjuarez@iu.edu>
2020-01-31 14:23:09 -0500

    Change log #4, added more dynamic features to the menu bar, have also added the sub accordion feature and removed unness duplicate script - ajjuarez

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/tables', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.send('Tables shown here temp:');
 });
 
