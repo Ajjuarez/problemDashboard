6008934c3796d8b3c93e88e2ab0220600963990f
Lex Juarez <ajjuarez@iu.edu>
2020-02-11 14:21:35 -0500

    Change Log #9, added modal feature, readmore feature, tab feature, and some styling in the styles.css. Also added a new style css to override styles for the modal

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -63,3 +63,46 @@ function toggleGraphsFunction(){
 }
 
 
+// Tabs - from https://rudrastyh.com/javascript/tabs.html
+function rudrSwitchTab(rudr_tab_id, rudr_tab_content) {
+	// first of all we get all tab content blocks (I think the best way to get them by class names)
+	var x = document.getElementsByClassName("tabcontent");
+	var i;
+	for (i = 0; i < x.length; i++) {
+		x[i].style.display = 'none'; // hide all tab content
+	}
+	document.getElementById(rudr_tab_content).style.display = 'block'; // display the content of the tab we need
+ 
+	// now we get all tab menu items by class names (use the next code only if you need to highlight current tab)
+	var x = document.getElementsByClassName("tabmenu");
+	var i;
+	for (i = 0; i < x.length; i++) {
+		x[i].className = 'tabmenu'; 
+	}
+	document.getElementById(rudr_tab_id).className = 'tabmenu active';
+}
+
+
+// Insert text into modal 
+	var content = $('.content').html();
+	var theDiv = $('.insert').html(content);
+	//need to figure out how to do this dynamically
+	
+
+// Readmore- https://stackoverflow.com/questions/38296509/trim-text-using-jquery-with-clickable-more-but-keep-text-format
+	var minimized_elements = $('.content'); 
+
+	minimized_elements.each(function() {
+	var t = $(this).html();
+	//set the length of small note here
+	if (t.length < 200) return;
+
+	$(this).html(
+		t.slice(0, 200) + '<a href="#ex1" rel="modal:open" class="more">Read More</a>' 
+	);
+	// the problem might be here with id='#ex1' - i think it needs to call on a different href 
+	// in order to display a different text in the modal
+
+	});
+
+
